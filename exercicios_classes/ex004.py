c4925123ccadff593e65a2c0408f27d16f05559b
Gabriel Iuri <gabrielsantos031bh@gmail.com>
2024-03-04 19:12:46 -0300

    Ex004

--- exercicios_classes/ex004.py
+++ exercicios_classes/ex004.py
@@ -11,6 +11,8 @@ Classe Pessoa: Crie uma classe que modele uma pessoa:
 Obs: Por padrão, a cada ano que nossa pessoa envelhece, sendo a idade dela menor que 21 anos, ela deve crescer 0,5 cm.
 '''
 
+import datetime
+
 
 class Pessoa:
     def __init__(self, nome: str, idade: int, peso: float, altura: float) -> None:
@@ -18,9 +20,13 @@ class Pessoa:
         self.idade = idade
         self.peso = peso
         self.altura = altura
+        self._ano_atual = datetime.datetime.now().date()
 
-    def envelhercer(self):
-        pass
+    def envelhercer(self) -> None:
+        self.idade += 1
+
+        if self.idade < 21:
+            self.altura += 0.005
 
     def engordar(self, pesos_engordar: float) -> None:
         if pesos_engordar > 0:
